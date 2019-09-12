309fd588b94e04338bef3cd6b2fd0af21d077880
oreanroy <oreanroy@gmail.com>
2019-09-12 23:37:10 +0530

    improved the code to a fucntional approach

--- scan.py
+++ scan.py
@@ -1,25 +1,44 @@
 import socket
 import subprocess
 
-hostname = socket.gethostname()
-IPAddr = socket.gethostbyname(hostname)
-
 
 all_up_host = []
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-print(s.getsockname()[0])
-network_ip = s.getsockname()[0]
-s.close()
+# get host ip, adress fucntion
+def get_host_name():
+  return socket.gethostname()
 
-print(" your computer name is: %r" % hostname)
-print(" and the ip adress in %r." % IPAddr)
-print(" and the ip adress on network is %s" % network_ip)
+def get_ip_normal():
+  return socket.gethostbyname(get_host_name())
+
+# virtual machines might give wrogng ip in that case this fucntion used  
+def get_ext_ip():
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  s.connect(("8.8.8.8", 80))
+  print(s.getsockname()[0])
+  network_ip = s.getsockname()[0]
+  s.close()
+  return network_ip
+
+
+print(" your computer name is: %r" % get_host_name())
+print(" and the ip adress in %r." % get_ip_normal())
+print(" and the ip adress on network is %s" % get_ext_ip())
+
+network_ip = get_ext_ip()
 
 print(network_ip[0:7])
 
-addrr = network_ip[0:7]
+
+# get the first three terms of ip adress
+def split_ip(ip):
+  terms = ip.split('.')
+  return terms[0:3]
+
+
+# sends a simple icmp packet and checks if the host is up
+def normal_scan(ip):
+  return subprocess.call(['ping', '-c', '3', adress]) and soc_test(adress)
 
 
 def soc_test(add):
@@ -41,8 +60,14 @@ def soc_test(add):
     return 2
 
 for i in range(0, 25):
-  adress = addrr+str(i)
-  res = subprocess.call(['ping', '-c', '3', adress]) and soc_test(adress)
+  adress=""
+  split = split_ip(network_ip)
+  for terms in split:
+    adress=adress+"."+terms
+    adress = adress[1:]
+  print adress
+  adress = adress+"."+str(i)
+  res = normal_scan(adress)
   if res == 0:
     print ("host %r is ok" % adress)
     all_up_host.append(adress)
@@ -53,4 +78,4 @@ for i in range(0, 25):
 
 print ("all the host up are.")
 
-print all_up_host 
+print ("all_up_host") 
