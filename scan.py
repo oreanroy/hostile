e2ed2d61103400ba29e914283695c2859d8ee6a4
oreanroy <oreanroy@gmail.com>
2019-09-13 00:05:52 +0530

    major improvements, in ui and fcuntional programming

--- scan.py
+++ scan.py
@@ -4,6 +4,16 @@ import subprocess
 
 all_up_host = []
 
+print ('''
+ __    __    ______        _______.___________. __   __       _______ 
+|  |  |  |  /  __  \      /       |           ||  | |  |     |   ____|
+|  |__|  | |  |  |  |    |   (----`---|  |----`|  | |  |     |  |__   
+|   __   | |  |  |  |     \   \       |  |     |  | |  |     |   __|  
+|  |  |  | |  `--'  | .----)   |      |  |     |  | |  `----.|  |____ 
+|__|  |__|  \______/  |_______/       |__|     |__| |_______||_______|
+                                                                      
+                                                                      .......................''')
+
 # get host ip, adress fucntion
 def get_host_name():
   return socket.gethostname()
@@ -11,7 +21,7 @@ def get_host_name():
 def get_ip_normal():
   return socket.gethostbyname(get_host_name())
 
-# virtual machines might give wrogng ip in that case this fucntion used  
+# virtual machines might give virtual/internal dhcp ip in that case this fucntion used  
 def get_ext_ip():
   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   s.connect(("8.8.8.8", 80))
@@ -20,14 +30,13 @@ def get_ext_ip():
   s.close()
   return network_ip
 
-
-print(" your computer name is: %r" % get_host_name())
-print(" and the ip adress in %r." % get_ip_normal())
-print(" and the ip adress on network is %s" % get_ext_ip())
+print ('\n \n \n')
+print("your computer name is: %r" % get_host_name())
+print("and the ip adress is %r" % get_ip_normal())
+print("and the ip adress on network is %s" % get_ext_ip())
+print ('\n \n \n')
 
 network_ip = get_ext_ip()
-
-print(network_ip[0:7])
 
 
 # get the first three terms of ip adress
@@ -38,9 +47,28 @@ def split_ip(ip):
 
 # sends a simple icmp packet and checks if the host is up
 def normal_scan(ip):
-  return subprocess.call(['ping', '-c', '3', adress]) and soc_test(adress)
+  return subprocess.call(['ping', '-c', '3', ip]) 
 
 
+# this fcuntion goes through all possible ip adress on the network and tries to ping it
+def run_scan_normal():
+  for i in range(0, 25):
+    adress=""
+    split = split_ip(network_ip)
+    for terms in split:
+      adress=adress+terms+"."
+    adress = adress+str(i) # construct the ip
+    res = normal_scan(adress)
+    if res == 0:
+      print ("host %r is ok" % adress)
+      all_up_host.append(adress)
+    if res == 2:
+      print ("host %r is down" % adress)
+    else:
+      print ("host %r does not exist" % adress)
+
+
+# this fuction goes for detailed scan just incase the machine do not respond to unkown icmp requests
 def soc_test(add):
   print ("the socket fucntion was called")
   ports = [20,21,22,23,25,80,111,443,445,631,993,995,135,137,138,139,548,631]
@@ -59,23 +87,8 @@ def soc_test(add):
   else:
     return 2
 
-for i in range(0, 25):
-  adress=""
-  split = split_ip(network_ip)
-  for terms in split:
-    adress=adress+"."+terms
-    adress = adress[1:]
-  print adress
-  adress = adress+"."+str(i)
-  res = normal_scan(adress)
-  if res == 0:
-    print ("host %r is ok" % adress)
-    all_up_host.append(adress)
-  if res == 2:
-    print ("host %r is down" % adress)
-  else:
-    print ("host %r does not exist" % adress)
+
+run_scan_normal()
 
 print ("all the host up are.")
-
-print ("all_up_host") 
+print (all_up_host) 
